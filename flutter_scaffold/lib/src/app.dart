b116fe72af363d00e8d8fe9ded9a6c6c9474f7bc
jcarlos <jcori.transoft@gmail.com>
2019-06-25 14:45:38 -0400

    demo about flutter

--- flutter_scaffold/lib/src/app.dart
+++ flutter_scaffold/lib/src/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'ui/movie_list.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: MovieList(),
+      ),
+    );
   }
-
 }
